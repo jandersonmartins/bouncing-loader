e783b50160ac06eafe6cdfa4771d4c985e729e52
Janderson Martins <jandersonmartins10@gmail.com>
2020-08-03 18:16:01 -0300

    fix dependencies and example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { BouncingLoader } from '@jandersonmartins/bouncing-loader'
-import '@jandersonmartins/bouncing-loader/dist/index.css'
 
 const App = () =>
   <BouncingLoader color="#3700B3" />
